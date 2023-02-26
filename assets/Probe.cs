55a90c1fc936e174637883088933aed9016aae44
H Grasso <aitch.grasso@gmail.com>
2023-02-26 05:27:12 -0600

    level 1 template

---
+++ assets/Probe.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Probe : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float vInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vInput = Input.GetAxisRaw("Vertical");
+        transform.Translate(Vector2.up * vInput * moveSpeed * Time.deltaTime);
+    }
+}
